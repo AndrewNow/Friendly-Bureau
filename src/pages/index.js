6f4539520cbe9b108cb06415fe4a7ee82039cd76
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2020-07-04 13:01:52 -0400

    added home screen randomization

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,34 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import misu from '../images/misu.jpg'
 import '../layouts/style.css'
 import { GridWrapper, } from '../components/ProductGrid/styles.js'
-
 import SEO from '~/components/seo'
 import ProductGrid from '~/components/ProductGrid'
+
+function getImage() {
+  let randomImages = [
+    '/images/misu.jpg',
+    '/images/misu2.jpg'
+  ];
+  let rand = Math.floor(Math.random() * (randomImages.length - 0) + 0)
+  return (
+    <img src={randomImages[rand]} alt="misu header photo"></img>
+  );
+}
 const IndexPage = () => (
   <>
-    <SEO title="Home" keywords={[`Montreal clothing brand`, `friendlybureau`, `toronto clothing brand`, `local screenprint`, `friendly bureau store`, `friendlybureau store`, `montreal graphic design`, `react`]} />
-
-    <div className="homeflexwrapper">
+    <SEO title="Home" keywords={[`Montreal clothing brand, friendlybureau, toronto clothing brand, local screenprint, friendly bureau store, friendlybureau store, montreal graphic design, react`]} />
+    <div className="homeflexwrapper" >
       <div className="misubackground">
-        <img src={require('../images/misu.jpg')} />
-      </div> 
+        {getImage()}
+      </div>
       <ProductGrid />
     </div>
-
-
-
     {/* <div class="heythere">
-      <h1>Hey there 👋</h1>
-      <p>Welcome to Friendly bureau.</p>
-    </div> */}
-
-
+  <h1>Hey there 👋</h1>
+  <p>Welcome to Friendly bureau.</p>
+</div> */}
     {/* <Link to="/page-2/">Go to page 2</Link> */}
   </>
 )
-
 export default IndexPage
